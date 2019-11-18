35700123ed32a39f2c678cc008459e96b3a1f257
Niek125 <niek.sleddens@gmail.com>
2019-11-18 11:33:02 +0100

    lowercase controller

--- src/main/java/plus/planner/messageservice/Controllers/MessageController.java
+++ src/main/java/plus/planner/messageservice/Controllers/MessageController.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequestMapping("MessageService")
+@RequestMapping("messageservice")
 @RestController
 public class MessageController {
     @Autowired
